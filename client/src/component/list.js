8c9ce756ee77a90499e920af0c8df31cc9a459f4
Aman kumar <87026675+Aman300@users.noreply.github.com>
2022-08-17 18:49:48 +0530

    done

--- client/src/component/list.js
+++ client/src/component/list.js
@@ -1,50 +1,61 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import '../App.css'
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 function List() {
 
     const [users, setUsers] = useState([]);
+    const [loader, setLoader] = useState(false);
 
     useEffect(() => {
         getData()
     }, []);
 
-    async function getData(){
-        const response  = await fetch('/showDbData')
-        const data = await response.json()
-        setUsers(data)
+    async function getData() {
+        setLoader(true);
+        setTimeout(async () => {
+            const response = await fetch('/showDbData')
+            const data = await response.json()
+            setUsers(data)
+            setLoader(false)
+        }, 500);
     }
 
-      //-----------------------------------
+    //-----------------------------------
 
- function onDelete(id){
-    fetch('/postDelete/'+id,{
-      method: 'delete',
-        headers: {
-          accept: 'application/json',
-        },
-    }).then((res) =>{
-      res.json().then((resp) =>{
-        alert('Document is deleted successfull')
-      }).then(res => getData())
-    })
-  }
+    function onDelete(id) {
+        fetch('/postDelete/' + id, {
+            method: 'delete',
+            headers: {
+                accept: 'application/json',
+            },
+        }).then((res) => {
+            res.json().then((resp) => {
+                alert('Document is deleted successfull')
+            }).then(res => getData())
+        })
+    }
 
     return (
 
         <>
-        <br></br>
-        <br></br>
-        <br></br>
-            <div className='container text-center'>
             <br></br>
+            <br></br>
+            <br></br>
+            <div className='container text-center'>
+                <br></br>
 
                 <h2>Employe data record</h2>
                 <br></br>
                 <table className="table table-responsive table-bordered  w-auto bg-white shadow">
+                    {loader ? <div className="d-flex justify-content-center p-5 m-5">
+                        <div className="spinner-border" role="status"> 
+                            <span className="visually-hidden"></span>
+                        </div>
+                    </div>:
                     <thead>
                         <tr>
                             <th scope="col">S.no</th>
@@ -56,8 +67,8 @@ function List() {
                             <th scope="col">state</th>
                             <th scope="col">zip</th>
                         </tr>
-                    </thead> {
-                        users.map((item,index) => (
+                    </thead> }{
+                        users.map((item, index) => (
                             <tbody>
                                 <tr>
                                     <td>{index + 1}</td>
@@ -69,12 +80,13 @@ function List() {
                                     <td>{item.state}</td>
                                     <td>{item.zip}</td>
 
-                                    <td><Link className='btn text-white' to={'/edit/'+(item._id)} >edit</Link></td>
+                                    <td><Link className='btn text-white' to={'/edit/' + (item._id)} >edit</Link></td>
                                     <td><button className='btn text-white' onClick={() => onDelete(item._id)} >delete</button></td>
                                 </tr>
                             </tbody>
                         ))
                     }
+                
                 </table>
             </div>
             <br></br>
